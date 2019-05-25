14071a4ab12d10e3125c16a9dd2f84fece632ad2
chenmeisong <chenmeisong@baidu.com>
2019-05-25 13:39:48 +0800

    init

---
+++ M4PanTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M4PanTransition
+//
+//  Created by Chen,Meisong on 2019/5/25.
+//  Copyright © 2019 xyz.chenms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
